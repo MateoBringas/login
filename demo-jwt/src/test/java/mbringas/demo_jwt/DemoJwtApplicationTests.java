27487fbb086d641b7466291f49db5a3b7398c7f8
Mateo <mateobringas6@gmail.com>
2024-08-20 09:52:22 -0300

    Iniciando proyecto

---
+++ demo-jwt/src/test/java/mbringas/demo_jwt/DemoJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package mbringas.demo_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
